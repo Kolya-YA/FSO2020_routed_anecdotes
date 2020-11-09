cd754d7fbb1838fc781e4dcbee78a24538b023f4
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2020-11-09 20:54:41 +0100

    7.1: routed anecdotes, step1

---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Anecdote = ({ anecdote }) => (
+  <li>
+    {anecdote.content}
+  </li>
+)
+
+const AnecdoteList = ({ anecdotes }) => (
+  <div>
+    <h2>Anecdotes</h2>
+    <ul>
+      {anecdotes.map(anecdote => <Anecdote key={anecdote.id} anecdote={anecdote} />)}
+    </ul>
+  </div>
+)
+
+export default AnecdoteList
